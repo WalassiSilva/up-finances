72ee80c203ceb3dbabcb04e8d852ec51ddee8774
Walassi Silva <walassi.b@gmail.com>
2024-12-30 22:50:22 -0300

    chore: add shadcn setup

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function TransactionsPage() {
-  return <div>TransactionsPage</div>;
+  return <div>TransactionsPage
+    <Button>Add transaction</Button>
+  </div>;
 }
