f9632d78a8098e0c75a25dc054162043b8f93b42
Walassi Silva <walassi.b@gmail.com>
2025-01-14 21:54:48 -0300

    feat: ensure user is redirectd to stripe checkout when acquiring plan

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/nav-bar";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { auth } from "@clerk/nextjs/server";
+import { CheckIcon, XIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -10,8 +12,59 @@ export default async function SubscriptionPage() {
   }
 
   return (
-    <main>
+    <>
       <Navbar />
-    </main>
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
+
+        <div className="flex gap-6">
+          <Card className="w-[450px]">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Básico
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Apenas 10 transações por mês (7/10)</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <XIcon />
+                <p>Relatórios de IA</p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card className="w-[450px]">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Premium
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">19</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Apenas 10 transações por mês (7/10)</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Relatórios de IA</p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
